e503e0327514227497d27f219da025afdb8e4188
rodrigotroy <1328268+rodrigo-troy@users.noreply.github.com>
2023-03-18 13:39:05 -0300

    Stage 3

--- Steganography and Cryptography/task/src/cryptography/Main.kt
+++ Steganography and Cryptography/task/src/cryptography/Main.kt
@@ -8,9 +8,8 @@ import javax.imageio.ImageIO
 fun main() {
     while (true) {
         println("Task (hide, show, exit):")
-        val input = readln()
 
-        when (input) {
+        when (val input = readln()) {
             "hide" -> hide()
             "show" -> show()
             "exit" -> {
@@ -50,7 +49,6 @@ fun hide() {
     println("Message to hide:")
     val message = readln()
 
-    //check if input file exists
     if (!File(inputImageFile).exists()) {
         println("Can't read input file!")
         return
@@ -60,7 +58,6 @@ fun hide() {
         println("The input image is not large enough to hold this message.")
         return
     }
-
 
     saveImageWithMessage(inputImageFile, outputImageFile, message)
 
@@ -84,20 +81,19 @@ fun isImageBigEnough(inputImageFile: String, message: String): Boolean {
 fun saveImageWithMessage(inputImageFile: String, outputImageFile: String, message: String) {
     val image: BufferedImage = ImageIO.read(File(inputImageFile))
 
-    val userMessage = (message.map { it.code } + listOf(0, 0, 3)).joinToString("") {
-        it.toString(2).padStart(8, '0')
-    }
+    val binaryMessage: String = message.encodeToByteArray().toMutableList()
+        .apply { addAll(listOf(0.toByte(), 0.toByte(), 3.toByte())) }
+        .joinToString("") { it.toString(2).padStart(8, '0') }
 
     var index = 0
-
-    outerLoop@ for (y in 0 until image.height) {
-        for (x in 0 until image.width) {
-            if (index == userMessage.length) {
+    (0 until image.height).forEach { y ->
+        (0 until image.width).forEach { x ->
+            if (index == binaryMessage.length) {
                 ImageIO.write(image, "png", File(outputImageFile))
                 return
             }
 
-            val bit = userMessage[index++].digitToInt()
+            val bit = binaryMessage[index++].digitToInt()
             val originalColor = Color(image.getRGB(x, y))
             val newColor = Color(originalColor.red, originalColor.green, originalColor.blue.and(254).or(bit))
             image.setRGB(x, y, newColor.rgb)
